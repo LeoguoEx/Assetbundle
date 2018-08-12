977d2d788c76d6da6d9be6f618e5d7308e1f618f
LeoguoEx <903587975@qq.com>
2018-08-13 04:38:46 +0800

    update assetbundle

---
+++ Assetbundle/Assets/Scripts/Editor/BuildAssetbundle.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class BuildAssetbundle 
+{
+	[MenuItem("Assets/Build Assetbudles")]
+	static void BuildAssetbundles()
+	{
+		string path = string.Format("{0}/{1}", Application.dataPath, "Assetbundle");
+		if (!Directory.Exists(path))
+		{
+			Directory.CreateDirectory(path);
+		}
+		BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.iOS);
+		AssetDatabase.Refresh();
+		
+	}
+
+	[MenuItem("Assets/LZMABundle")]
+	static void BuildLZMABundle()
+	{
+		string path = string.Format("{0}/{1}", Application.dataPath, "LZMABundle");
+		if (!Directory.Exists(path))
+		{
+			Directory.CreateDirectory(path);
+		}
+
+		BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.iOS);
+		AssetDatabase.Refresh();
+	}
+
+	[MenuItem("Assets/LZ4Bundle")]
+	static void BuildLZ4Bundle()
+	{
+		string path = string.Format("{0}/{1}", Application.dataPath, "LZ4Bundle");
+		if (!Directory.Exists(path))
+		{
+			Directory.CreateDirectory(path);
+		}
+
+		BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.iOS);
+		AssetDatabase.Refresh();
+	}
+
+	[MenuItem("Assets/NoneCompressBundle")]
+	static void BuildNoneBundle()
+	{
+		string path = string.Format("{0}/{1}", Application.dataPath, "NoneCompressedBundle");
+		if (!Directory.Exists(path))
+		{
+			Directory.CreateDirectory(path);
+		}
+
+		BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.iOS);
+		AssetDatabase.Refresh();
+	}
+}
